bd3a9b8b8ab321966797cc0437a1bc3e58fb20c3
fannyf123 <fannyespakar123@gmail.com>
2026-05-15 22:51:57 +0700

    Revert "Redesign total animasi EP1: single unified animation engine"
    This reverts commit f9c2ee0fdce07c1faf15bed28ab357449d8b7670.

--- js/main.js
+++ js/main.js
@@ -5,8 +5,17 @@
 
 import '../css/index.css';
 import './particles.js';
+import './scroll-experience.js';
+import './section-transitions.js';
 import './artefak.js';
-import { initEp1Animations } from './ep1-animations.js';
+import { initProfilAnimation } from './profil-animation.js';
+import { initPendidikanAnimation } from './pendidikan-animation.js';
+import { initArtefakAnimation } from './artefak-animation.js';
+import { initModelGuruAnimation } from './model-guru-animation.js';
+import { initKeahlianAnimation } from './keahlian-animation.js';
+import { initGaleriAnimation } from './galeri-animation.js';
+import { initKontakAnimation } from './kontak-animation.js';
+import { initFooterAnimation } from './footer-animation.js';
 import { initPortfolioChooser } from './portfolio-chooser.js';
 import { initSertifikatModal } from './sertifikat-modal.js';
 import { refreshEp2Animation } from './eportfolio2-animation.js';
@@ -617,7 +626,14 @@ function initMain() {
     });
   });
 
-  initEp1Animations();
+  initProfilAnimation();
+  initPendidikanAnimation();
+  initArtefakAnimation();
+  initModelGuruAnimation();
+  initKeahlianAnimation();
+  initGaleriAnimation();
+  initKontakAnimation();
+  initFooterAnimation();
   initPortfolioChooser();
   initSertifikatModal();
 
